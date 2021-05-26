459cbc0bebef977a5f4cafb41387f5d82b7f483c
Emma Bradford <ebradford@Emmas-MacBook-Air.local>
2021-05-26 09:05:48 -0500

    finished project

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -9,13 +9,13 @@ public class BinarySearchTree<T extends Comparable<T>> implements WordCountColle
 {
     //constructors
     BinaryNode<T> root = null;
-    
+
     // Returns the number of elements in this collection.
     public int size()
     {
         return size(root);
     }
-    
+
     private int size(BinaryNode<T> r)
     {
         if(r == null)
@@ -27,37 +27,93 @@ public class BinarySearchTree<T extends Comparable<T>> implements WordCountColle
             return 1 + size(r.getLeft()) + size(r.getRight());
         }
     }
-    
+
     // Returns true if this collection is empty; otherwise, returns false.
     public boolean isEmpty()
     {
-        return root ==null;
+        return root == null;
     }
 
     // Adds element to this collection.
     // Precondition: element is not already in the collection
     public void add (T element)
     {
-    
+        root = add(element, root);
     }
-    
+
+    public BinaryNode<T> add(T element, BinaryNode<T> tree)
+    {
+        if(tree == null)
+        {
+            BinaryNode ans = new BinaryNode<T>(element);
+            tree = ans;
+        }
+        else if(element.compareTo(tree.getInfo()) <= 0)
+        {
+            tree.setLeft(add(element, tree.getLeft()));
+        }
+        else
+        {
+            tree.setRight(add(element, tree.getRight()));
+        }
+        return tree;
+    }
     // Returns the element of this colletion if contains an element e such that
     // e.compareTo(element) == 0; otherwise, returns null.
     public T fetch(T element)
     {
-        return null;
+        BinaryNode<T> a = fetch(element, root);
+        if (a==null)
+        {
+            return null;
+        }
+        else
+        {
+            return a.getInfo();
+        }
     }
 
+    public BinaryNode<T> fetch(T element, BinaryNode<T> tree)
+    {
+        if(tree == null)
+        {
+            return null;
+        }
+        else if(element.compareTo(tree.getInfo()) < 0)
+        {
+            return fetch(element, tree.getLeft());
+        } 
+        else if(element.compareTo(tree.getInfo()) > 0)
+        {
+            return fetch(element, tree.getRight());
+        }
+        else
+        {
+            return tree;
+        }
+    }
     // Removes all elements in this collection
     public void clear()
     {
-    
+        root = null;
     }
-    
+
     // generates a list of the elements in the collection
     // Postcondion: list is independant (deep copy) of original collection
     public List<T> createList()
     {
-        return null;
+        List<T> a = new ArrayList<T>();
+        inorderTraverse(root, a);
+        return a;
+    }
+    
+    private void inorderTraverse(BinaryNode<T> tree, List list)
+    {
+        if(tree != null)
+        {
+            inorderTraverse(tree.getLeft(), list);
+            list.add(tree.getInfo());
+            inorderTraverse(tree.getRight(), list);
+        }
     }
 }
--- LinkedList.java
+++ LinkedList.java
@@ -8,44 +8,69 @@ import java.util.*;
 public class LinkedList<T extends Comparable<T>> implements WordCountCollection<T>
 {
     //constructors
-    
+    private LLNode<T> front;
     
     // Returns the number of elements in this collection.
     public int size()
     {
-        return 0;
+        LLNode<T> curr = front;
+        int size = 0;
+        while(curr != null)
+        {
+            size++;
+            curr = curr.getLink();
+        }
+        return size;
     }
     
     // Returns true if this collection is empty; otherwise, returns false.
     public boolean isEmpty()
     {
-        return false;
+        return front == null;
     }
 
     // Adds element to this collection.
     // Precondition: element is not already in the collection
     public void add (T element)
     {
-    
+        LLNode<T> newN = new LLNode(element);
+        newN.setLink(front);
+        front = newN;
     }
     
     // Returns the element of this colletion if contains an element e such that
     // e.compareTo(element) == 0; otherwise, returns null.
     public T fetch(T element)
     {
+        LLNode<T> curr = front;
+        while(curr != null)
+        {
+            if(element.compareTo(curr.getInfo()) == 0)
+            {
+                return curr.getInfo();
+            }
+            curr = curr.getLink();
+        }
         return null;
     }
 
     // Removes all elements in this collection
     public void clear()
     {
-    
+        front = null;
     }
     
     // generates a list of the elements in the collection
     // Postcondion: list is independant (deep copy) of original collection
     public List<T> createList()
     {
-        return null;
+        LLNode<T> curr = front;
+        List<T> a = new ArrayList<T>();
+        while(curr != null)
+        {
+            a.add(curr.getInfo());
+            curr = curr.getLink();
+        }
+        return a;
     }
 }
